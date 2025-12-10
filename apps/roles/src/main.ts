c2a1167c417fb2ffdf6a824f1d586c8e0ca7b719
kida21 <kidusp22@gmail.com>
2025-12-10 10:55:10 +0300

    Dockerize & orm module setuped

--- apps/roles/src/main.ts
+++ apps/roles/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { RolesModule } from './roles.module';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(RolesModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService)
+  const port = configService.get<string>('HTTP_PORT')
+  if (!port){
+    throw new Error('failed to load Port from .env')
+  }
+  await app.listen(port);
 }
 bootstrap();
--- apps/roles/src/roles.module.ts
+++ apps/roles/src/roles.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Role } from 'apps/entities/role.entity';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory:(configService : ConfigService) => ({
+        type:'postgres',
+        host:configService.get<string>('HOST'),
+        port:configService.get<number>('PORT'),
+        database:configService.get<string>('DATABASE'),
+        username:configService.get<string>('USERNAME'),
+        password:configService.get<string>('PASSWORD'),
+        entities:[Role],
+        synchronize:true
+        
+      }),
+      inject:[ConfigService]
+    })
+  ],
   controllers: [RolesController],
   providers: [RolesService],
 })
