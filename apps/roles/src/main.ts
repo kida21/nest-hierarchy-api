920671a6dd6153bc204336f8dc41707c48086b77
kida21 <kidusp22@gmail.com>
2025-12-15 08:26:28 +0300

    cors enabled

--- apps/roles/src/main.ts
+++ apps/roles/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(RolesModule);
   const configService = app.get(ConfigService)
   const port = configService.get<string>('HTTP_PORT')
+  const url = configService.get('URL')
   if (!port){
     throw new Error('failed to load Port from .env')
   }
@@ -18,6 +19,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: url, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true,
+  });
   await app.listen(port);
 }
 bootstrap();
