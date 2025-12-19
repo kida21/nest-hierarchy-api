99242257d901eb42e086fafe8044d0e4ab04722f
kida21 <kidusp22@gmail.com>
2025-12-19 09:03:58 +0300

    update on main user

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService)
+  const port = configService.get<number>('USER_HTTP_PORT')
+  if (!port){
+    throw new Error('failed to load Port from .env')
+  }
+  await app.listen(port);
 }
 bootstrap();
