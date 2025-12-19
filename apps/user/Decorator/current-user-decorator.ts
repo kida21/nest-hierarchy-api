bb8226ba2a4b313ff143c329860e78a0583f3127
kida21 <kidusp22@gmail.com>
2025-12-19 11:13:45 +0300

    decorator

---
+++ apps/user/Decorator/current-user-decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { User } from "../entities/user.entity";
+
+const getCurrentUser = (context:ExecutionContext):User =>{
+    return context.switchToHttp().getRequest().user
+}
+
+export const CurrentUser = createParamDecorator((_data:unknown,context:ExecutionContext)=>getCurrentUser(context))
