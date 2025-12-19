cc88b5098183f6f9963b46cfb4949d000a73dfe0
kida21 <kidusp22@gmail.com>
2025-12-19 09:57:53 +0300

    update on user-service

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/create-user-dto';
+import * as bcrypt from 'bcryptjs'
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectRepository(User) private readonly userRepository:Repository<User>){}
+
+  async createUser(userDto:CreateUserDto){
+    return this.userRepository.create({
+      ...userDto,
+      password: await bcrypt.hash(userDto.password,10)
+    })
+  }
+
+  async verifyUser(email:string,password:string){
+     const user = await this.userRepository.findOneBy({email})
+     if(!user){
+      throw new NotFoundException('user not found')
+     }
+     const passwordIsValid = bcrypt.compare(password,user.password)
+     if(!passwordIsValid){
+        throw new UnauthorizedException('Credentials are invalid')
+     }
+     return user
   }
 }
