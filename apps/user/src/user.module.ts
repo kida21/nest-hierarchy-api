a78365057e426349ce63bd2e573e6289bc5cf5c2
kida21 <kidusp22@gmail.com>
2025-12-23 08:28:31 +0300

    update on user module

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -3,9 +3,25 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forRootAsync({
+       inject: [ConfigService],
+       useFactory:(configService:ConfigService)=>({
+        type:'postgres',
+        host: configService.get<string>('HOST'),
+        port: configService.get<number>('PORT'),
+        database: configService.get<string>('POSTGRES_DB'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        entities: [User],
+        synchronize: false,
+       })
+    }),
     JwtModule.registerAsync({
       useFactory:(configService:ConfigService) => ({
         secret:configService.get<string>('JWT_SECRET'),
